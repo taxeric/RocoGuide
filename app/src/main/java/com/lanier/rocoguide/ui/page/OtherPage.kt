dd210864143d3fad2dceb9d4ebd5ee0f47aca38c
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2022-09-11 21:47:30 +0800

    [update]: 功能页UI调整

--- app/src/main/java/com/lanier/rocoguide/ui/page/OtherPage.kt
+++ app/src/main/java/com/lanier/rocoguide/ui/page/OtherPage.kt
@@ -1,7 +1,5 @@
 package com.lanier.rocoguide.ui.page
 
-import android.content.Intent
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -12,26 +10,15 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
-import com.lanier.lib_net.RetrofitHelper
 import com.lanier.rocoguide.R
 import com.lanier.rocoguide.base.ROUTE_SCREEN_MAIN_GENETIC
 import com.lanier.rocoguide.base.ROUTE_SCREEN_MAIN_PERSONALITY
 import com.lanier.rocoguide.base.ROUTE_SCREEN_MAIN_SETTINGS
 import com.lanier.rocoguide.base.ROUTE_SCREEN_MAIN_SKILL_LIST
-import com.lanier.rocoguide.base.cache.LocalCache
-import com.lanier.rocoguide.service.DownloadService
 import com.lanier.rocoguide.ui.common.*
-import com.lanier.rocoguide.utils.NotificationUtil
-import com.lanier.rocoguide.utils.installApk
 
 /**
  * Create by Eric
@@ -45,7 +32,7 @@ fun OtherScreen(navHostController: NavHostController, title: String){
         }) {
             Icon(imageVector = Icons.Outlined.Settings, contentDescription = "")
         }
-    }) {
+    }, titleBarSize = TitleBarSize.Medium) {
         OthersMain(navHostController, padding = it)
     }
 }
@@ -60,7 +47,6 @@ fun OthersMain(navHostController: NavHostController, padding: PaddingValues){
 @Composable
 fun OtherCS(navHostController: NavHostController){
     Column(modifier = Modifier.fillMaxWidth()) {
-        SingleTitle(title = "CS", dividerHeight = 0.5f)
         OtherHorizontalItem("技能大全") {
             navHostController.navigate(ROUTE_SCREEN_MAIN_SKILL_LIST)
         }
