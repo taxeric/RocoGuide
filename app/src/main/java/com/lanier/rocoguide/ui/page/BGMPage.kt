99ebfe3d85a60520ff4be654690383e4e7b6f9ac
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2022-09-18 20:35:31 +0800

    [update]: BGM子项UI调整

--- app/src/main/java/com/lanier/rocoguide/ui/page/BGMPage.kt
+++ app/src/main/java/com/lanier/rocoguide/ui/page/BGMPage.kt
@@ -1,25 +1,31 @@
 package com.lanier.rocoguide.ui.page
 
 import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavHostController
 import com.lanier.rocoguide.base.cache.LocalCache
 import com.lanier.rocoguide.entity.RemoteMusicEntity
 import com.lanier.rocoguide.service.music.MusicService
 import com.lanier.rocoguide.ui.common.EnableBackBaseScaffold
 import com.lanier.rocoguide.ui.theme.ExtendedTheme
+import com.lanier.rocoguide.R
 
 /**
  * Create by Eric
@@ -61,14 +67,23 @@ fun BGMScreenList(){
 @Composable
 fun BGMScreenItem(music: RemoteMusicEntity){
     val context = LocalContext.current
-    Text(text = music.name, modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)
-        .clickable {
+        /*.clickable {
             LocalCache.bgmList.clear()
             LocalCache.bgmList.add(music)
             context.startService(
                 Intent(context, MusicService::class.java)
             )
-        })
+        }*/
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .clickable {}
+        .padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = painterResource(id = R.drawable.ic_outline_music_note), contentDescription = "",
+            modifier = Modifier.weight(1f))
+        Text(text = music.name, modifier = Modifier.weight(3f))
+        IconButton(onClick = {  }, modifier = Modifier.weight(1f)) {
+            Icon(painter = painterResource(id = R.drawable.ic_outline_cloud_download),
+                contentDescription = "")
+        }
+    }
 }
