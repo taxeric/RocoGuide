535e1cdce42e5f54f9f18200a6c4cf4f71fb4090
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2023-01-15 21:46:03 +0800

    [feat]: 新增设置服务端对话框

--- app/src/main/java/com/lanier/rocoguide/ui/common/Dialog.kt
+++ app/src/main/java/com/lanier/rocoguide/ui/common/Dialog.kt
@@ -82,7 +82,7 @@ fun SimpleDialog(
                 modifier = Modifier
                     .align(Alignment.End)
                     .padding(10.dp, 2.dp)) {
-                Text(text = "OK")
+                Text(text = stringResource(id = R.string.ok))
             }
             Spacer(modifier = Modifier.height(5.dp))
         }
@@ -146,7 +146,11 @@ fun SpiritFilterDialog(onDismiss: () -> Unit){
                         color = if (selected == tab.tag) ExtendedTheme.colors.defaultRacialGridBgColor else MaterialTheme.colorScheme.onSurface,
                         fontSize = 12.sp,
                         modifier = childModifier
-                            .border(1.dp, if (selected == tab.tag) ExtendedTheme.colors.defaultRacialGridBgColor else Color.Transparent, RoundedCornerShape(4.dp))
+                            .border(
+                                1.dp,
+                                if (selected == tab.tag) ExtendedTheme.colors.defaultRacialGridBgColor else Color.Transparent,
+                                RoundedCornerShape(4.dp)
+                            )
                             .padding(2.dp)
                     )
                 }
@@ -208,7 +212,7 @@ fun SearchDialog(type: Search, label: String = "", onDismiss: (String) -> Unit) 
                     onClick = { onDismiss("") }, modifier = Modifier
                         .padding(5.dp)
                 ) {
-                    Text(text = "取消")
+                    Text(text = stringResource(id = R.string.cancel))
                 }
                 TextButton(
                     onClick = { onDismiss(mDesc) }, modifier = Modifier
@@ -235,15 +239,15 @@ fun DarkThemeDialog(onDismiss: () -> Unit){
                 .background(MaterialTheme.colorScheme.background)
         ) {
             Spacer(modifier = Modifier.height(8.dp))
-            SingleChoiceItem(text = "跟随系统", modifier = Modifier.padding(10.dp, 0.dp),
+            SingleChoiceItem(text = stringResource(id = R.string.follow_system), modifier = Modifier.padding(10.dp, 0.dp),
                 selected = darkValue == SettingsHelper.PreferenceDarkTheme.FOLLOW_SYSTEM){
                 darkValue = SettingsHelper.PreferenceDarkTheme.FOLLOW_SYSTEM
             }
-            SingleChoiceItem(text = "Day", modifier = Modifier.padding(10.dp, 0.dp),
+            SingleChoiceItem(text = stringResource(id = R.string.day), modifier = Modifier.padding(10.dp, 0.dp),
                 selected = darkValue == SettingsHelper.PreferenceDarkTheme.OFF){
                 darkValue = SettingsHelper.PreferenceDarkTheme.OFF
             }
-            SingleChoiceItem(text = "Dark", modifier = Modifier.padding(10.dp, 0.dp),
+            SingleChoiceItem(text = stringResource(id = R.string.dark), modifier = Modifier.padding(10.dp, 0.dp),
                 selected = darkValue == SettingsHelper.PreferenceDarkTheme.ON){
                 darkValue = SettingsHelper.PreferenceDarkTheme.ON
             }
@@ -253,7 +257,7 @@ fun DarkThemeDialog(onDismiss: () -> Unit){
                     SettingsHelper.switchDarkThemeMode(darkTheme.darkThemeValue)
                     onDismiss()
                 }, modifier = Modifier) {
-                    Text(text = "取消")
+                    Text(text = stringResource(id = R.string.cancel))
                 }
                 TextButton(onClick = {
                     onDismiss()
@@ -280,19 +284,19 @@ fun RacialValueDialog(onDismiss: () -> Unit) {
         ) {
             Spacer(modifier = Modifier.height(8.dp))
             SingleChoiceItem(
-                text = "表格", modifier = Modifier.padding(10.dp, 0.dp),
+                text = stringResource(id = R.string.grid), modifier = Modifier.padding(10.dp, 0.dp),
                 selected = localStyle == PreferenceUtil.RACIAL_GRID
             ) {
                 localStyle = PreferenceUtil.RACIAL_GRID
             }
             SingleChoiceItem(
-                text = "进度", modifier = Modifier.padding(10.dp, 0.dp),
+                text = stringResource(id = R.string.progress), modifier = Modifier.padding(10.dp, 0.dp),
                 selected = localStyle == PreferenceUtil.RACIAL_PROGRESS
             ) {
                 localStyle = PreferenceUtil.RACIAL_PROGRESS
             }
             SingleChoiceItem(
-                text = "六边形", modifier = Modifier.padding(10.dp, 0.dp),
+                text = stringResource(id = R.string.hexagonal), modifier = Modifier.padding(10.dp, 0.dp),
                 selected = localStyle == PreferenceUtil.RACIAL_HEXAGONAL
             ) {
                 localStyle = PreferenceUtil.RACIAL_HEXAGONAL
@@ -302,7 +306,7 @@ fun RacialValueDialog(onDismiss: () -> Unit) {
                 TextButton(onClick = {
                     onDismiss()
                 }, modifier = Modifier) {
-                    Text(text = "取消")
+                    Text(text = stringResource(id = R.string.cancel))
                 }
                 TextButton(onClick = {
                     PreferenceUtil.updateInt(PreferenceUtil.RACIAL_SHOW_STYLE, localStyle)
@@ -338,48 +342,16 @@ fun EggDialog(groupName: String, groupId: Int, onDismiss: (Boolean) -> Unit){
                 append(stringResource(id = R.string.lay_egg_tips))
             }, modifier = Modifier.fillMaxWidth())
             Spacer(modifier = Modifier.height(20.dp))
-            Row(modifier = Modifier.align(Alignment.End)
+            Row(
+                modifier = Modifier
+                    .align(Alignment.End)
             ) {
                 TextButton(onClick = { onDismiss(false) }, modifier = Modifier) {
-                    Text(text = "取消")
+                    Text(text = stringResource(id = R.string.cancel))
                 }
                 TextButton(onClick = { onDismiss(true) }, modifier = Modifier) {
                     Text(text = stringResource(id = R.string.sure))
                 }
-            }
-            Spacer(modifier = Modifier.height(5.dp))
-        }
-    }
-}
-
-@Composable
-fun DataErrorDialog(type: Int, onDismiss: () -> Unit) {
-    Dialog(
-        onDismissRequest = onDismiss,
-        properties = DialogProperties(
-            dismissOnClickOutside = true,
-            dismissOnBackPress = true
-        )
-    ) {
-        Column(modifier = Modifier
-            .clip(RoundedCornerShape(10.dp))
-            .background(MaterialTheme.colorScheme.background)) {
-            val errorStr = when (type) {
-                -1 -> "编号对应精灵错误"
-                -3 -> "精灵获取失败"
-                -4 -> "服务器响应异常"
-                else -> "其他异常"
-            }
-            Text(text = "数据可能有误: $errorStr", modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp))
-            Spacer(modifier = Modifier.height(20.dp))
-            TextButton(
-                onClick = onDismiss, modifier = Modifier
-                    .align(Alignment.End)
-                    .padding(5.dp)
-            ) {
-                Text(text = stringResource(id = R.string.sure))
             }
             Spacer(modifier = Modifier.height(5.dp))
         }
@@ -435,7 +407,7 @@ fun SpiritDetailsFixDialog(onDismiss: () -> Unit){
             TextButton(onClick = onDismiss, modifier = Modifier
                 .align(Alignment.End)
                 .padding(5.dp, 2.dp)) {
-                Text(text = stringResource(id = R.string.sure))
+                Text(text = stringResource(id = R.string.ok))
             }
             Spacer(modifier = Modifier.height(5.dp))
         }
@@ -452,7 +424,7 @@ fun GeneticDialog(onDismiss: (Int) -> Unit){
             .clip(RoundedCornerShape(5.dp))
             .background(MaterialTheme.colorScheme.background)) {
             Text(
-                text = "调试",
+                text = stringResource(id = R.string.debug),
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(8.dp)
@@ -465,7 +437,7 @@ fun GeneticDialog(onDismiss: (Int) -> Unit){
             TextButton(onClick = { onDismiss(-1) }, modifier = Modifier
                 .align(Alignment.End)
                 .padding(5.dp)) {
-                Text(text = "Cancel")
+                Text(text = stringResource(id = R.string.cancel))
             }
             Spacer(modifier = Modifier.height(5.dp))
         }
@@ -538,7 +510,6 @@ fun SkillDialog(onDismiss: () -> Unit){
  * 不建议在 [onNegativeEvent] 和 [onPositiveEvent] 方法块内进行dismiss操作
  * 对话框的消失只建议在 [onDismiss] 方法块执行
  */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChangeLogDialog(
     log: String,
@@ -576,10 +547,12 @@ fun ChangeLogDialog(
         ) {
             Spacer(modifier = Modifier.height(10.dp))
             Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "版本更新",
+                Text(
+                    text = stringResource(id = R.string.upgrade_version),
                     color = MaterialTheme.colorScheme.onBackground,
                     fontSize = 19.sp,
-                    fontWeight = FontWeight.Bold,)
+                    fontWeight = FontWeight.Bold,
+                )
                 Spacer(modifier = Modifier.width(10.dp))
                 Image(painter = painterResource(id = R.drawable.ic_lulu_normal_1), contentDescription = "",
                     modifier = Modifier.size(36.dp))
@@ -588,7 +561,7 @@ fun ChangeLogDialog(
             Text(text = buildAnnotatedString {
                 appendInlineContent("star")
                 withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
-                    append("更新内容:")
+                    append(stringResource(id = R.string.upgrade_content))
                 }
             }, fontSize = 14.sp, color = MaterialTheme.colorScheme.primary, inlineContent = inlineContentMap)
             Text(text = buildAnnotatedString {
@@ -610,7 +583,7 @@ fun ChangeLogDialog(
             Text(text = buildAnnotatedString {
                 appendInlineContent("star")
                 withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
-                    append("文件大小:")
+                    append(stringResource(id = R.string.file_size))
                 }
                 withStyle(SpanStyle(color = MaterialTheme.colorScheme.onBackground)) {
                     append(size)
@@ -620,7 +593,7 @@ fun ChangeLogDialog(
             if (!mandatory) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Checkbox(checked = checkEnable, onCheckedChange = { checkEnable = it })
-                    Text(text = "本版不再提示", fontSize = 13.sp, color = MaterialTheme.colorScheme.onBackground,
+                    Text(text = stringResource(id = R.string.no_more_tips), fontSize = 13.sp, color = MaterialTheme.colorScheme.onBackground,
                         modifier = Modifier
                             .press {
                                 checkEnable = !checkEnable
@@ -636,12 +609,12 @@ fun ChangeLogDialog(
                     onDismiss()
                 }) {
                     val negativeButton = if (isDownloading)
-                        "关闭"
+                        stringResource(id = R.string.close)
                     else {
                         if (mandatory) {
-                            "退出"
+                            stringResource(id = R.string.exit)
                         } else {
-                            "稍后"
+                            stringResource(id = R.string.later)
                         }
                     }
                     Text(text = negativeButton)
@@ -653,13 +626,101 @@ fun ChangeLogDialog(
                     onDismiss()
                 }, enabled = !isDownloading) {
                     val downloadStr = if (isDownloading) {
-                        "正在下载"
+                        stringResource(id = R.string.downloading)
                     } else {
-                        "立即下载"
+                        stringResource(id = R.string.download_now)
                     }
                     Text(text = downloadStr)
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ConfigDialog(
+    onDismiss: (Boolean) -> Unit
+) {
+    var host by remember {
+        mutableStateOf("127.0.0.1")
+    }
+    var port by remember {
+        mutableStateOf("8080")
+    }
+    var alwaysShowTips by remember {
+        mutableStateOf(false)
+    }
+    Dialog(
+        onDismissRequest = { onDismiss(false) },
+        properties = DialogProperties(
+            dismissOnClickOutside = true,
+            dismissOnBackPress = true
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(5.dp))
+                .background(MaterialTheme.colorScheme.background)
+                .padding(12.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.dialog_title_set_serve_address),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .fillMaxWidth()
+                    .padding(0.dp, 12.dp)
+            )
+            OutlinedTextField(
+                value = host,
+                onValueChange = { host = it },
+                label = {
+                    Text(text = stringResource(id = R.string.host))
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = port,
+                onValueChange = { port = it },
+                label = {
+                    Text(text = stringResource(id = R.string.port))
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Checkbox(checked = alwaysShowTips, onCheckedChange = { alwaysShowTips = it })
+                Text(
+                    text = stringResource(id = R.string.no_more_tips),
+                    modifier = Modifier
+                        .clickable {
+                            alwaysShowTips = !alwaysShowTips
+                        }
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                TextButton(onClick = { onDismiss(false) }, modifier = Modifier) {
+                    Text(text = stringResource(id = R.string.cancel))
+                }
+                TextButton(onClick = { onDismiss(true) }, modifier = Modifier) {
+                    Text(text = stringResource(id = R.string.sure))
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
