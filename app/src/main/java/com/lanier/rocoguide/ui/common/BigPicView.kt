bdf9d189eb1ad0f0af899c661e9fb3cdb5e585ea
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2023-03-30 22:36:07 +0800

    [update]: 触摸事件完善

--- app/src/main/java/com/lanier/rocoguide/ui/common/BigPicView.kt
+++ app/src/main/java/com/lanier/rocoguide/ui/common/BigPicView.kt
@@ -2,11 +2,14 @@ package com.lanier.rocoguide.ui.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 
@@ -19,6 +22,15 @@ fun CommonBigPicView(
     navHostController: NavHostController,
     url: String?
 ) {
+    var scale by remember {
+        mutableStateOf(1f)
+    }
+    var transX by remember {
+        mutableStateOf(0f)
+    }
+    var transY by remember {
+        mutableStateOf(0f)
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -32,6 +44,28 @@ fun CommonBigPicView(
                     .fillMaxSize()
                     .clickable {
                         navHostController.popBackStack()
+                    }
+                    .graphicsLayer(
+                        translationX = transX,
+                        translationY = transY,
+                        scaleX = scale,
+                        scaleY = scale,
+                    )
+                    .pointerInput(Unit) {
+                        detectTransformGestures { centroid, pan, zoom, rotation ->
+                            scale = when {
+                                scale < 1f -> 1f
+                                scale > 3f -> 3f
+                                else -> scale * zoom
+                            }
+                            if (scale > 1f) {
+                                transX += (pan.x * scale)
+                                transY += (pan.y * scale)
+                            } else {
+                                transX = 0f
+                                transY = 0f
+                            }
+                        }
                     }
             )
         }
